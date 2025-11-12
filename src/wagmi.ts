ffbc7fcf294f8dd434b61b80844a0ec7b29e6eec
huynhlalyla <phantruchuynh190404@gmail.com>
2025-11-12 14:19:20 +0700

    lỗi1

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -19,8 +19,8 @@ const rootstockTestnet = {
     symbol: 'tRBTC',
   },
   rpcUrls: {
-    default: { http: ['https://rpc.testnet.rootstock.io/'] },
-    public: { http: ['https://rpc.testnet.rootstock.io/'] },
+    default: { http: ['https://public-node.testnet.rsk.co'] },
+    public: { http: ['https://public-node.testnet.rsk.co'] },
   },
   blockExplorers: {
     default: { name: 'RSK Explorer', url: 'https://explorer.testnet.rsk.co' },
